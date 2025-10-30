3728ab91ce1a3b9f919ba575e06dacd564533be5
Banana-4 <stig34768@gmail.com>
2025-10-30 18:00:34 +0100

    Create 9.PalindromeNumber.c
    Solution for the leetcode problem num 9.

---
+++ two-pointers/9.PalindromeNumber.c
@@ -0,0 +1,19 @@
+#include <math.h>
+bool isPalindrome(int x) {
+    if (x < 0) {
+        return false;
+    }
+    int base = 10;
+    long left = (long)pow(base, (int)log10(x));
+    long right = base;
+
+    int left_dig, right_dig; 
+    while (x) {
+        left_dig = x / left;
+        right_dig = x % base;
+        x = (x % left) / right;
+        if (left_dig != right_dig) return false;
+        left /= 100;
+    }
+    return true;
+}
