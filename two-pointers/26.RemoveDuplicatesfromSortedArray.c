58e27cbd47be4759da67c429148cbba6dd92d133
Banana-4 <stig34768@gmail.com>
2025-11-04 10:24:25 +0100

    add leetcode 26

---
+++ two-pointers/26.RemoveDuplicatesfromSortedArray.c
@@ -0,0 +1,8 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int count = 0;
+    for (int i = 1; i < numsSize; ++i) {
+        if (nums[i] == nums[i - 1]) ++count;
+        nums[i - count] = nums[i];
+    }
+    return numsSize - count;
+}
