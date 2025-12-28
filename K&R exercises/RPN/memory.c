50030cbf0dd8d2dfe9d3ee9242951fc83636a0f2
Banana-4 <stig34768@gmail.com>
2025-12-28 21:32:24 +0100

    add memory.c

---
+++ K&R exercises/RPN/memory.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define MAXVAL 100 /* maximum depth of val stack */
+
+int sp = 0; /* stack pointer */
+double val[MAXVAL]; /* value stack */
+
+int inUse[26] = {0};
+double vars[26] {0};
+
+double push(f) /* push f onto value stack */
+double f;
+{
+  if (sp < MAXVAL)
+    return(val[sp++] = f);
+  else {
+    printf("error: stack full\n");
+    clear();
+    return(0);
+  }
+}
+
+double pop() /* pop top value from stack */
+{
+  if (sp > 0)
+    return(val[--sp]);
+  else {
+    printf("error: stack empty\n");
+    clear();
+    return(0);
+  }
+}
+
+clear() /* clear stack */
+{
+  sp = 0;
+}
+
+void store_val(char name,double f) {
+	
+}
