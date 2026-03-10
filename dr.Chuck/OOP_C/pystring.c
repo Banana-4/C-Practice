5d29cdd73445b1d5db68391ec9c16c70fb667e43
Banana-4 <stig34768@gmail.com>
2026-03-10 13:02:02 +0000

    add guard for realloc

--- dr.Chuck/OOP_C/pystring.c
+++ dr.Chuck/OOP_C/pystring.c
@@ -20,6 +20,9 @@ struct pystring *append(struct pystring *s, char c) {
   if (s->len == s->size) {
       s->size *= 2;
       s->string = realloc(s->string, s->size);
+        if (!s->string) {
+          return NULL;
+      }
   }
   s->string[s->len++] = c;
   s->string[s->len] = '\0';
