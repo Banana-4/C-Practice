87ff5dff39199f23497fa544e1dfa6854d963df7
Banana-4 <stig34768@gmail.com>
2026-03-19 09:30:23 +0000

    add  word count

---
+++ dr.Chuck/Chapter 1/word_count.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define YES 1
+#define NO  0
+
+main() /* count lines, words, chars in input */
+{
+    int c, nl, nw, nc, inword;
+
+    inword = NO;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF) {
+        ++nc;
+        if (c == '\n' )
+            ++nl;
+        if (!isalpha(c) && !isdigit(c) && c != '\'')
+            inword = NO;
+        else if ( inword == NO && isalpha(c) ) {
+            inword = YES;
+            ++nw;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
+}
+
