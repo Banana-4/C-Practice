a605f787eff4701a2f1ab086d91aa614a3104eab
Banana-4 <stig34768@gmail.com>
2026-05-09 17:44:42 +0000

    Improve search logic in rotated sorted array
    Refactor binary search logic for target in rotated array.

--- LeetCode/BinSearch/33.SearchinRotatedSortedArray.c
+++ LeetCode/BinSearch/33.SearchinRotatedSortedArray.c
@@ -9,28 +9,22 @@ int search(int* nums, int numsSize, int target) {
     int *pivot = NULL;
     while (left <= right) {
         int *mid = left + (right - left) / 2;
-        if (last >= *mid) {
-            pivot = mid;
-            right = mid - 1;
-        } else {
-            left = mid + 1;
-        }
-    }
-    if (target > last) {
-        left = nums;
-        right = pivot -1;
-    } else {
-        left = pivot;
-        right = nums + numsSize - 1;
-    }
-    while (left <= right) {
-        int *mid = left + (right - left) / 2;
-        if (target == *mid) {
+        if (*mid == target)
             return (int)(mid - nums);
-        } else if (target > *mid) {
-            left = mid + 1;
+        bool sameSide = (*mid > last) == (target > last);
+        if (sameSide) {
+            if (*mid < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
         } else {
-             right = mid - 1; 
+            if (target > last) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
     }
     return NOT_FOUND;
